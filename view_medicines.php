98bf10dc57671abf782049b1c3f15b32f62d3ae1
Yahya Mohamed <yelnouby26@yahoo.com>
2021-05-15 17:37:50 +0300

    Add: Pharmacist Pages

---
+++ view_medicines.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Hospital System</title>
+
+  </head>
+  <body class="bg">
+
+    <div class="container-fluid p-0">
+
+      <!-- HEADER -->
+      <div class="row">
+        <nav class="navbar navbar-light header px-0">
+          <div class="container-fluid">
+            <a class="navbar-brand">Welcome Dr. Ibrahim </a>
+            <form class="d-flex">
+              <a href="logout.php" class="btn btn-danger" type="submit">Logout</a>
+            </form>
+          </div>
+        </nav>
+      </div>
+
+      <!-- Prescribe Patient -->
+      <div class="row">
+
+        <div class="m-4 text-center">
+          <h2 class="h2 mb-3">MEDICINIES</h2>
+        </div>
+
+        <div class="col-12 col-md-8 mx-auto bg-form p-5 rounded">
+          <div class="row text-center">
+            <form class="d-flex">
+            <table class="table table-sm table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Medicine ID</th>
+                  <th scope="col">Medicine Name</th>
+                  <th scope="col">Medicine Type</th>
+                  <th scope="col">QTY</th>
+                  <th scope="col">Add</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>5</td>
+                  <td>Parcetmol</td>
+                  <td>Tablets</td>
+                  <td>10</td>
+                  <td><a href="#" class="btn btn-danger p-2">Add More</a></td>
+                </tr>
+                <tr>
+                <th scope="row">1</th>
+                  <td>15</td>
+                  <td>Claritine</td>
+                  <td>Syrup</td>
+                  <td>5</td>
+                  <td><a href="#" class="btn btn-danger p-2">Add More</a></td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>25</td>
+                  <td>Augmentin</td>
+                  <td>Tablets</td>
+                  <td>1</td>
+                  <td><a href="#" class="btn btn-danger p-2">Add More</a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <div class="mx-auto px-100 my-3">
+            <nav aria-label="...">
+              <ul class="pagination">
+                <li class="page-item disabled">
+                  <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item active" aria-current="page">
+                  <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#">Next</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div class="col-12 text-center mt-3">
+          <a href="doctor_home.php" class="btn btn-danger p-2">Return</a>
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
