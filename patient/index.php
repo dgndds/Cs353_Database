6175b3881577b6aa0ce665a0758512ac53b9d7b7
dgndss <dogancan1998231@gmail.com>
2021-05-16 19:52:27 +0300

    Appointment implementation started

--- patient/index.php
+++ patient/index.php
@@ -1,11 +1,11 @@
 <?php
 
-  require_once("config.php");
+  require_once("../config.php");
 
   session_start();
 
   if ( !(isset($_SESSION["TC"]) && $_SESSION["type"] == "patient") ) {
-    header("location:index.php");
+    header("location:../index.php");
   }
 
 ?>
@@ -20,7 +20,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 
     <title>Hospital System</title>
 
@@ -35,7 +35,7 @@
           <div class="container-fluid">
             <a class="navbar-brand">Welcome Doctor Hakan Kara</a>
             <form class="d-flex">
-              <a href="logout.php" class="btn btn-danger" type="submit">Logout</a>
+              <a href="../logout.php" class="btn btn-danger" type="submit">Logout</a>
             </form>
           </div>
         </nav>
---
+++ patient/patient_book_appointment.php
@@ -0,0 +1,241 @@
+<?php
+  require_once("../config.php");
+  session_start();
+  
+  if ( !(isset($_SESSION["TC"]) && $_SESSION["type"] == "patient") ) {
+    header("location:../index.php");
+  }
+
+  try{
+    $connection = new PDO("mysql:host=" . $GLOBALS['host'] . "; dbname=" . $GLOBALS['database'], $GLOBALS['username'], $GLOBALS['password']);
+
+    $query = $connection->prepare("SELECT department_name FROM departments");
+
+    $query->execute();
+    
+    if(isset($_POST['doctorId']) && isset($_POST['chosenDate'])){
+      $selectedDoctor = $_POST['doctorId'];
+      $selectedDate = $_POST['chosenDate'];
+      $userTc = $_POST['TC'];
+
+      echo 'You have chosen: ' . $selectedDoctor;
+      echo 'You have chosen: ' . $selectedDate;
+      echo 'Your tc: '. $userTc;
+      //INSERT INTO time_shift (TC, shift_date, available) VALUES (?, ?, 'reserved');
+      $query = $connection->prepare("INSERT INTO time_shift (TC, shift_date, available) VALUES (?, ?, 'reserved');");
+
+      $query->execute(
+        array(
+          $selectedDoctor,$selectedDate
+        )
+      );
+    }
+
+    if(isset($_SESSION["selectedDeparment"]) && isset($_SESSION["selectedMonth"])){
+      $selectedDepartment= $_SESSION["selectedDeparment"];
+      $selectedMonth = $_SESSION["selectedMonth"]; 
+    }
+    
+
+    if(!empty($_POST['department-name']) && !empty($_POST['month'])) {
+      $selectedDepartment = $_POST['department-name'];
+      $selectedMonth = $_POST['month'];
+      $_SESSION["selectedDeparment"] =$selectedDepartment;
+      $_SESSION["selectedMonth"] = $selectedMonth;
+
+      echo 'You have chosen: ' . $selectedDepartment;
+      echo 'You have chosen: ' . $selectedMonth;
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+
+    <title>Hospital System</title>
+
+  </head>
+  <body class="bg">
+
+    <div class="container-fluid p-0">
+
+      <!-- HEADER -->
+      <div class="row">
+        <nav class="navbar navbar-light header px-0">
+          <div class="container-fluid">
+            <a class="navbar-brand">Welcome Hakan Kara</a>
+            <form class="d-flex">
+              <a href="logout.php" class="btn btn-danger" type="submit">Logout</a>
+            </form>
+          </div>
+        </nav>
+      </div>
+
+      <!-- Prescribe Patient -->
+      <div class="row">
+
+        <div class="m-4 text-center">
+          <h2 class="h2">TEST RESULT HISTORY</h2>
+        </div>
+
+        <div class="col-12 col-md-8 mx-auto mb-4">
+          <div class="row">
+            <div class="col-12 col-md-4 mb-3 text-center">
+              <div class="dropdown col-12 col-md-6">
+                <p class="d-inline" style="font-size:1.3rem;"><b>Month: </b></p>
+                <form action="patient_book_appointment.php" method="post">
+                  <select name="month" class="form-select">
+                      <option value="" disabled selected>Choose option</option>
+                      <option value="01">January</option>
+                      <option value="02">February</option>
+                      <option value="03">March</option>
+                      <option value="04">April</option>
+                      <option value="05">May</option>
+                      <option value="06">June</option>
+                      <option value="07">July</option>
+                      <option value="08">August</option>
+                      <option value="09">September</option>
+                      <option value="10">October</option>
+                      <option value="11">November</option>
+                      <option value="12">December</option>
+                  </select>
+              </div>
+            </div>
+            <div class="col-12 col-md-4 mb-3 text-center">
+              <div class="dropdown col-12 col-md-6">
+                <p class="d-inline" style="font-size:1.3rem;"><b>Department: </b></p>
+                  <select name="department-name" class="form-select">
+                      <option value="" disabled selected>Choose option</option>
+                      <?php
+                        while($data = $query->fetch()){?>
+                          <option value="<?=$data["department_name"]?>"><?=$data["department_name"]?></option>
+                          <?php } 
+                      ?>
+                  </select>
+              </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <input class="btn btn-danger" type="submit" value="Search">
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <?php
+          $query = $connection->prepare("SELECT department_name FROM departments");
+          $query->execute();
+        ?>
+
+        <div class="col-12 col-md-8 mx-auto bg-form p-5 rounded">
+          <div class="row text-center">
+            <div class="col-12">
+              <table class="table table-sm table-striped table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Doctor Name</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+
+                   $query = $connection->prepare("SELECT TC,first_name,last_name from doctor natural join user where TC in (select TC from  departments natural join works_in where department_name=? )");
+
+                   $query->execute(
+                     array(
+                       $selectedDepartment
+                     )
+                   );
+
+                   $doctors = $query;
+                   while($data = $doctors->fetch()){
+                     for($i = 1; $i <= 30; $i++){
+                      //If size is greater than zero than that date is reserved
+                      //SELECT * FROM time_shift where '2021-05-19' in (select shift_date from time_shift) and shift_date='2021-05-19';
+                      $i_padded = sprintf("%02d", $i);
+                      $monthPadded = sprintf("%02d", $selectedMonth);
+                      $date = "2021-" . $monthPadded ."-". $i_padded;
+  
+                      $query = $connection->prepare("SELECT * FROM time_shift where ? in (select shift_date from time_shift) and shift_date=? and TC=?");
+  
+                      $query->execute(
+                        array(
+                          $date,$date,$data["TC"]
+                        )
+                      );
+                      
+                      if ($query->rowCount() == 0 ){
+                        $dateToShow = $i."-".$selectedMonth."-2021";
+                        echo "<form action=\"patient_book_appointment.php\" method=\"post\">";
+                        echo "<tr>";
+                        echo "<th scope=\"row\">$i</th>";
+                        echo "<td><input type=\"hidden\" name=\"doctorId\" value=\"".$data["TC"]."\">".$data["first_name"]." ".$data["last_name"]."</td>";
+                        echo "<td><input type=\"hidden\" name=\"chosenDate\" value=\"".$date."\">".$dateToShow."</td>";
+                        echo "<td><input type=\"submit\" value=\"Appoint\"></td>";
+                        echo "</tr>";
+                        echo "</form>";
+                      }
+                    }
+                   }
+                ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <div class="mx-auto px-100 my-3">
+            <nav aria-label="...">
+              <ul class="pagination">
+                <li class="page-item disabled">
+                  <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item active" aria-current="page">
+                  <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#">Next</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <?php
+          } catch (PDOException $err) {
+            echo "<h1>Cant Connect Database!</h1>";
+          }
+        ?>
+
+
+      </div>
+
+
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
